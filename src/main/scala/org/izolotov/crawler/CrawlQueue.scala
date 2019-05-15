d89873dd5dd7b7d984e215b12a4c3284bd76b827
Ivan Zolotov <izolotov@gryddinamics.com>
2019-05-15 23:52:33 +0300

    Introduce Crawl queue class

---
+++ src/main/scala/org/izolotov/crawler/CrawlQueue.scala
@@ -0,0 +1,87 @@
+package org.izolotov.crawler
+
+import java.net.URL
+import java.util.concurrent.{LinkedBlockingQueue, TimeUnit}
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.apache.commons.httpclient.HttpStatus
+import org.apache.http.entity.ContentType
+import org.apache.http.impl.client.CloseableHttpClient
+import org.izolotov.crawler.parser.ProductParserRepo
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+class CrawlQueue(urls: Iterable[UncrawledURL],
+                 httpClient: CloseableHttpClient,
+                 defaultFetchDelay: Long,
+                 fetchTimeout: Long) extends Iterator[ProductCrawlAttempt]{
+
+  import scala.compat.java8.OptionConverters._
+  import ExecutionContext.Implicits.global
+
+  val crawlCounter = new AtomicInteger(urls.size)
+  val resultQueue = new LinkedBlockingQueue[ProductCrawlAttempt]()
+
+  urls
+    .groupBy(unfetched => new URL(unfetched.url).getHost)
+    .mapValues(entry => entry.iterator)
+    .iterator
+    .foreach{ group =>
+    val future = Future{
+      val fetcher = new DelayFetcher(httpClient, defaultFetchDelay)
+      val host = group._1
+      group._2.map{ unfetched =>
+        val attempt = fetcher.fetch(unfetched.url, fetchTimeout)
+        attempt.getResponse.asScala
+          .map{
+            response =>
+              val content = response.getEntity.getContent
+              val responseCode = response.getStatusLine.getStatusCode
+              val doc: Option[Product] = if (responseCode == HttpStatus.SC_OK) {
+                Some(ProductParserRepo.parse(host, unfetched.url, content, ContentType.getOrDefault(response.getEntity).getCharset))
+              } else {
+                None
+              }
+              content.close()
+              response.close()
+              new ProductCrawlAttempt(unfetched.url, Some(responseCode), Option(attempt.getResponseTime.get), None, doc)
+          }
+          .getOrElse(
+            new ProductCrawlAttempt(
+              unfetched.url,
+              None,
+              attempt.getResponseTime.asScala.map(rt => rt.toLong),
+              attempt.getException.asScala.map(exc => exc.toString),
+              None
+            )
+          )
+      }
+    }
+    future onComplete {
+      case Success(crawled) => crawled.foreach{data =>
+        this.synchronized {
+          resultQueue.add(data)
+          crawlCounter.decrementAndGet()
+        }
+      }
+      case Failure(exc) =>
+        throw exc
+    }
+  }
+
+  override def hasNext: Boolean = {
+    this.synchronized {
+      return !(crawlCounter.get() == 0 && resultQueue.peek() == null)
+    }
+  }
+
+  override def next(): ProductCrawlAttempt = {
+    val next = resultQueue.poll(fetchTimeout * 2, TimeUnit.MILLISECONDS)
+    if (next != null) {
+      next
+    } else {
+      throw new RuntimeException("Looks like CrawlQueue being stuck")
+    }
+  }
+}
---
+++ src/test/scala/org/izolotov/crawler/CrawlQueueSpec.scala
@@ -0,0 +1,85 @@
+package org.izolotov.crawler
+
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+
+import org.apache.http.impl.client.HttpClients
+import org.eclipse.jetty.server.{Request, Server}
+import org.eclipse.jetty.server.handler.AbstractHandler
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import org.json4s.DefaultFormats
+import org.json4s.jackson.Serialization
+import CrawlQueueSpec._
+object CrawlQueueSpec {
+  val Port = 8082
+
+  val Host1PetzlLynx = s"http://localhost:${Port}/petzl-lynx"
+  val Host1BDCyborg = s"http://localhost:${Port}/dlack-diamond-cyborg"
+  val Host1PetzlVasak = s"http://localhost:${Port}/petzl-vasak"
+  val Host2GrivelRambo = s"http://127.0.0.1:${Port}/grivel-rambo"
+
+  object RequestHandler extends AbstractHandler {
+    override def handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse): Unit = {
+      implicit val formats = DefaultFormats
+      val dummyProduct =
+        Product(baseRequest.getRequestURL.toString, "store", "brand", "name", Seq("category"), 1F, None, Currency.Rub.toString, None)
+      response.setContentType("text/plain;charset=utf-8")
+      response.setStatus(HttpServletResponse.SC_OK)
+      response.getWriter.print(Serialization.write(dummyProduct))
+      baseRequest.setHandled(true)
+    }
+  }
+}
+
+class CrawlQueueSpec extends FlatSpec with BeforeAndAfter {
+
+  var server: Server = null
+
+  before {
+    server = new Server(Port)
+    server.setHandler(RequestHandler)
+    server.start()
+  }
+
+  after {
+    server.stop()
+  }
+
+  it should "crawl different hosts in parallel" in {
+    val uncrawled = Seq(
+      UncrawledURL(s"http://localhost:${Port}/product"),
+      UncrawledURL(s"http://127.0.0.1:${Port}/product")
+    )
+
+    val startTime = System.currentTimeMillis()
+    val crawled = new CrawlQueue(uncrawled, HttpClients.createDefault(), 300L, 1000L)
+      .map(crawled => (crawled, System.currentTimeMillis() - startTime)).toList
+    val elapsedTime = System.currentTimeMillis() - startTime
+
+    assert(crawled.map(_._2).reduce((et1, et2) => et1 + et2) > elapsedTime)
+  }
+
+  it should "crawl all provided urls" in {
+    val uncrawled = Seq(
+      UncrawledURL(s"http://localhost:${Port}/product"),
+      UncrawledURL(s"http://localhost:${Port}/product"),
+      UncrawledURL(s"http://localhost:${Port}/product"),
+      UncrawledURL(s"http://localhost:${Port}/product"),
+      UncrawledURL(s"http://127.0.0.1:${Port}/product"),
+      UncrawledURL(s"http://127.0.0.1:${Port}/product"),
+      UncrawledURL(s"http://127.0.0.1:${Port}/product")
+    )
+
+    val actual = new CrawlQueue(uncrawled, HttpClients.createDefault(), 100L, 1000L).toList
+    assert(actual.size == 7)
+    actual.foreach(
+      item =>
+        assert(item.document.get == Product(item.url, "store", "brand", "name", Seq("category"), 1F, None, Currency.Rub.toString, None))
+    )
+  }
+
+  it should "not fail if no urls provided" in {
+    val actual = new CrawlQueue(Seq(), HttpClients.createDefault(), 100L, 1000L).toList
+    assert(actual.size == 0)
+  }
+
+}

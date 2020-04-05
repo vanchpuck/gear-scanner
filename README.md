99082b5f3119098dc7ca247b3e0a68285ba30894
Ivan Zolotov <vanchpuck@mail.ru>
2020-04-05 23:02:20 +0300

    Simple configuration to run on ECS

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ![Build Status](https://codebuild.us-east-2.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoiNi9YVXJzRDUyaWtYUkVWS1NwUXh6MUo3QXdqVDU5SU1TRjdBdUlzYWFic25YVERnQXpHa29GTHZyampScVdDRnVST1Jpc1RISzVtWThwOUpBRnJtb1owPSIsIml2UGFyYW1ldGVyU3BlYyI6IkNMeEttNFdBdDBZa0RzcTMiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=master)
 ## The GearScanner workflow
 ![Workflow schema](http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/vanchpuck/gear-scanner/master/workflow.iuml?)
+
+## How to run with ECS
+`ecs-cli compose --project-name gear-scanner-crawler service up --create-log-groups --cluster-config GearScannerCrawlerConf --ecs-profile default`
--- src/main/scala/org/izolotov/crawler/DynamoDBHelper.scala
+++ src/main/scala/org/izolotov/crawler/DynamoDBHelper.scala
@@ -1,11 +1,10 @@
 package org.izolotov.crawler
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder
 import com.typesafe.scalalogging.Logger
 import org.scanamo.{DynamoFormat, Scanamo, Table}
-
 import DynamoDBHelper._
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 
 object DynamoDBHelper {
   private val Log = Logger[DynamoDBHelper]
@@ -13,7 +12,7 @@ object DynamoDBHelper {
 
 class DynamoDBHelper(tableName: String, region: String) {
 
-  private val client = AmazonDynamoDBAsyncClientBuilder.standard().withRegion(region).withCredentials(new ProfileCredentialsProvider()).build()
+  private val client = AmazonDynamoDBAsyncClientBuilder.standard().withRegion(region).withCredentials(new DefaultAWSCredentialsProviderChain()).build()
   private val scanamo = Scanamo(client)
 
   def save[A](data: A)(implicit format: DynamoFormat[A]): Unit = {
